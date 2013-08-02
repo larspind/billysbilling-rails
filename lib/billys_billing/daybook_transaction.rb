edd5992442f0b001acb6a317050b1a7c596f5b89
Bhushan Lodha <bhushanlodha@gmail.com>
2013-08-02 16:13:11 +0530

    added attributes to enable creating transaction

--- lib/billys_billing/daybook_transaction.rb
+++ lib/billys_billing/daybook_transaction.rb
@@ -1,10 +1,8 @@
 module BillysBilling
   class DaybookTransaction < BillysBilling::Base
-    attr_accessor :id, :created_time, :entry_date, :api_type, :description, :extended_description, :transaction_running_no,
-                  :postings, :url, :success
+    attr_accessor :id, :created_time, :entry_date, :api_type, :description, :extended_description, :transaction_running_no, :transaction_no,
+                  :postings, :url, :success, :reference_text, :postings
       
-    
     has_many :attachments
-
   end
 end
